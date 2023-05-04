359fbdfddb4241c2caa91aba8ace17ae58e0da21
alsduq1117 <alsduq1117@naver.com>
2023-05-05 00:45:19 +0900

    feat: GithubActions CI/CD 적용

--- src/main/java/com/idle/idle_backend/config/jwt/common/BasicController.java
+++ src/main/java/com/idle/idle_backend/config/jwt/common/BasicController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BasicController {
 
-    @GetMapping("/healthcheck")
+    @GetMapping("/")
     public String home() {
         return "<h1>idle-backend 스웨거 주소: http://awsv4-env.eba-mre2mcnv.ap-northeast-2.elasticbeanstalk.com/swagger-ui/index.html </h1>";
     }
